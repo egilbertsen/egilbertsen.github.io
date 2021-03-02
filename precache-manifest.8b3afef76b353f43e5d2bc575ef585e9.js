b7d488cd616615658f4e3677dd4989bd8e84801c
EG5924 <67492348+egilbertsen@users.noreply.github.com>
2021-03-02 12:45:56 -0800

    Updates

---
+++ precache-manifest.8b3afef76b353f43e5d2bc575ef585e9.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2153c6410ab1b087f95ad6f66c87b406",
+    "url": "/index.html"
+  },
+  {
+    "revision": "92ad50f86963f6dafe86",
+    "url": "/static/css/main.82488a9f.chunk.css"
+  },
+  {
+    "revision": "cd95d33833dcf23af7b0",
+    "url": "/static/js/2.0465c390.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.0465c390.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92ad50f86963f6dafe86",
+    "url": "/static/js/main.c5a2f8fb.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "6981ab04280463e2268875ff43c0b374",
+    "url": "/static/media/Bookshelf.6981ab04.png"
+  },
+  {
+    "revision": "04bd286f5e6ea4e7940572d7646a7b0e",
+    "url": "/static/media/NoteExample.04bd286f.png"
+  },
+  {
+    "revision": "d75dcc464efe6ff86b753dcbde982f1b",
+    "url": "/static/media/WeatherOrNot.d75dcc46.png"
+  },
+  {
+    "revision": "471596144442200a07ee8c8b73d424a8",
+    "url": "/static/media/bkg.47159614.jpeg"
+  },
+  {
+    "revision": "0ce71c33ea3167b8967bf5f8e8bd439e",
+    "url": "/static/media/portrait.0ce71c33.png"
+  }
+]);
